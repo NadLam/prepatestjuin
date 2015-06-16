0595184cd1d140b1b4158db52e7dd9e6b9a5911f
Nad <kastatan@hotmail.fr>
2015-06-16 16:17:19 +0200

    préparation du test3

---
+++ datas/mes-requetes.sql
@@ -0,0 +1,6 @@
+/* requête de connexion avec récupération des droits */
+SELECT  u.id, u.lemail, u.lenom, 
+		d.lenom AS nom_perm, d.ladesc, d.laperm 
+	FROM utilisateur u
+		INNER JOIN droit d ON u.droit_id = d.id
+    WHERE u.lelogin='Admin' AND u.lemdp = 'admin';
